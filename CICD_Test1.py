791e689603359ae9279b11a9984159c1a5334726
nshutijean <j.nshuti73@gmail.com>
2024-01-30 18:03:58 +0200

    add: a caption for the title

--- CICD_Test1.py
+++ CICD_Test1.py
@@ -9,6 +9,7 @@ from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
 
 st.set_page_config(page_title="Llama2 TGI Streaming ",layout="wide",)
 st.title ("Llama2 TGI Streaming Test 4")
+st.caption("This tests the Llama 2 Text Generation Interface")
 
 with stylable_container(
     key="top_content",
